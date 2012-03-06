0db902e2a17814789fe07f2d3ee1c2c4469dc841
DLavin23 <daniel.e.lavin@gmail.com>
2012-03-05 19:53:20 -0600

    added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@ class ArticlesController < ApplicationController
   def index
     @user = User.find_by_id session[:user_id]  
     @articles = Article.where("user_id = ?", "#{@user.id}")
+    @articles = Article.order('time_added asc').page(params[:page]).per(8)
+  
   end
 
   def show
